7a98e6488895e1fcf8c5f437a8450b7a9f719ae9
carlos <tuotramedicina@gmail.com>
2020-03-10 21:31:38 +0100

    estacionesData

--- app/src/main/java/com/riluca/healthyair/javabean/Estacion.java
+++ app/src/main/java/com/riluca/healthyair/javabean/Estacion.java
@@ -2,17 +2,31 @@ package com.riluca.healthyair.javabean;
 
 public class Estacion {  //Parcelable??
     private String id;
+    private String nombre;
+    private String direccion;
     private String longitud;
     private String latitud;
 
-    public Estacion(String id, String longitud, String latitud) {
+    public Estacion() {}
+
+    public Estacion(String id, String nombre, String direccion, String longitud, String latitud) {
         this.id = id;
+        this.nombre = nombre;
+        this.direccion = direccion;
         this.longitud = longitud;
         this.latitud = latitud;
     }
 
     public String getId() {
         return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
     }
 
     public String getLongitud() {
